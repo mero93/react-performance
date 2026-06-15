207fffa5de9b8011b1afb1f4e373a4a32b4ef1ed
mero93 <merabkopaleishvili1993@gmail.com>
2026-06-16 02:20:07 +0400

    refactor: wrap in memo, use useMemo for variables

--- src/components/country-card/country-card.tsx
+++ src/components/country-card/country-card.tsx
@@ -8,6 +8,7 @@ import {
 import { formatNumber } from '../../utils/format-utils';
 
 import styles from './country-card.module.css';
+import { memo, useMemo } from 'react';
 
 type CountryCardProps = {
   country: Country;
@@ -15,10 +16,18 @@ type CountryCardProps = {
   selectedColumns: string[];
 };
 
-export const CountryCard = ({ country, selectedYear, selectedColumns }: CountryCardProps) => {
-  const yearDataMap = createYearDataMap(country.data);
-  const population = getPopulationForYear(yearDataMap, selectedYear);
-  const co2 = getCo2ForYear(yearDataMap, selectedYear);
+export const CountryCard = memo(({ country, selectedYear, selectedColumns }: CountryCardProps) => {
+  const yearDataMap = useMemo(() => {
+    return createYearDataMap(country.data);
+  }, [country.data]);
+
+  const population = useMemo(() => {
+    return getPopulationForYear(yearDataMap, selectedYear);
+  }, [yearDataMap, selectedYear]);
+
+  const co2 = useMemo(() => {
+    return getCo2ForYear(yearDataMap, selectedYear);
+  }, [yearDataMap, selectedYear]);
 
   return (
     <div className={styles.card}>
@@ -39,4 +48,4 @@ export const CountryCard = ({ country, selectedYear, selectedColumns }: CountryC
       <DataTable data={country.data} year={selectedYear} columns={selectedColumns} />
     </div>
   );
-};
+});
